bc62c0ac09b751c6e21c45d74ecc9f9ea9a7c30f
Liukq <liukq1994@163.com>
2019-10-09 14:00:22 +0800

    添加modules-home

---
+++ app/src/main/java/com/haohaishengshi/haohaimusic/modules/home/HomeFragment.java
@@ -0,0 +1,18 @@
+package com.haohaishengshi.haohaimusic.modules.home;
+
+import com.zhiyicx.baseproject.base.TSFragment;
+
+/**
+ * Created by Liukq on 2019/10/9
+ */
+public class HomeFragment extends TSFragment {
+    @Override
+    protected int getBodyLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void setPresenter(Object presenter) {
+
+    }
+}
